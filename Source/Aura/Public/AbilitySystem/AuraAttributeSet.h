38315c40704dbb334975047f0ba1ab8545f68503
Guiu <guiur@hotmail.com>
2024-02-03 10:40:39 +0100

    Added health and mana attributes

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -14,4 +14,32 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	// Constructor
+	UAuraAttributeSet();
+
+	// Function needed for attributes replication to work
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/** Define all Attributes **/
+	// We want health to be replicated and also have a replication confrim from the server -> "ReplicateUsing=OnRep_Health"
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+
+	// UFUNCTION -> Decorator needed to be a replicate notification function used as callback
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
