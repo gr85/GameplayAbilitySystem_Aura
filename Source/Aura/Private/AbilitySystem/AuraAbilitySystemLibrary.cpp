68ecc5bb66cf0c4220bde9d7f52dcb339f8b95a2
Guiu <guiur@hotmail.com>
2024-02-18 12:03:12 +0100

    Add Execution Calculation for damage, taking in account Block Chance, Armor and Armor Penetration

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -46,13 +46,10 @@ UAttributeMenuWidegetController* UAuraAbilitySystemLibrary::GetAttributeMenuWidg
 void UAuraAbilitySystemLibrary::InitializeDefaultAttributes(const UObject* WorldContextObject, ECharacterClass CharacterClass, 
     float level, UAbilitySystemComponent* ASC)
 {
-    AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(WorldContextObject));
-    if (AuraGameMode == nullptr) return;
-
     AActor* AvatarActor = ASC->GetAvatarActor();
 
-    UCharacterClassInfo* CharacetClassInfo = AuraGameMode->CharacterClassInfo;
-    FCharacterClassDefaultInfo ClassDefaultInfo = CharacetClassInfo->GetClassDefaultInfo(CharacterClass);
+    UCharacterClassInfo* CharacterClassInfo = GetCharacterClassInfo(WorldContextObject);
+    FCharacterClassDefaultInfo ClassDefaultInfo = CharacterClassInfo->GetClassDefaultInfo(CharacterClass);
 
     FGameplayEffectContextHandle PrimaryAttributesContextHandle = ASC->MakeEffectContext();
     PrimaryAttributesContextHandle.AddSourceObject(AvatarActor);
@@ -61,24 +58,29 @@ void UAuraAbilitySystemLibrary::InitializeDefaultAttributes(const UObject* World
 
     FGameplayEffectContextHandle SecondaryAttributesContextHandle = ASC->MakeEffectContext();
     SecondaryAttributesContextHandle.AddSourceObject(AvatarActor);
-    const FGameplayEffectSpecHandle SecondaryAttributesSpecHandle = ASC->MakeOutgoingSpec(CharacetClassInfo->SecondaryAttributes, level, SecondaryAttributesContextHandle);
+    const FGameplayEffectSpecHandle SecondaryAttributesSpecHandle = ASC->MakeOutgoingSpec(CharacterClassInfo->SecondaryAttributes, level, SecondaryAttributesContextHandle);
     ASC->ApplyGameplayEffectSpecToSelf(*SecondaryAttributesSpecHandle.Data.Get());
 
     FGameplayEffectContextHandle VitalAttributesContextHandle = ASC->MakeEffectContext();
     VitalAttributesContextHandle.AddSourceObject(AvatarActor);
-    const FGameplayEffectSpecHandle VitalAttributesSpecHandle = ASC->MakeOutgoingSpec(CharacetClassInfo->VitalAttributes, level, VitalAttributesContextHandle);
+    const FGameplayEffectSpecHandle VitalAttributesSpecHandle = ASC->MakeOutgoingSpec(CharacterClassInfo->VitalAttributes, level, VitalAttributesContextHandle);
     ASC->ApplyGameplayEffectSpecToSelf(*VitalAttributesSpecHandle.Data.Get());
 }
 
 void UAuraAbilitySystemLibrary::GiveStartupAbilities(const UObject *WorldContextObject, UAbilitySystemComponent *ASC)
 {
-    AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(WorldContextObject));
-    if (AuraGameMode == nullptr) return;
-
-    UCharacterClassInfo* CharacterClassInfo = AuraGameMode->CharacterClassInfo;
+    UCharacterClassInfo* CharacterClassInfo = GetCharacterClassInfo(WorldContextObject);
     for (TSubclassOf<UGameplayAbility> AbilityClass : CharacterClassInfo->CommonAbilities)
     {
         FGameplayAbilitySpec AbilitySpec = FGameplayAbilitySpec(AbilityClass, 1);
         ASC->GiveAbility(AbilitySpec);
     }
+}
+
+UCharacterClassInfo* UAuraAbilitySystemLibrary::GetCharacterClassInfo(const UObject* WorldContextObject)
+{
+    AAuraGameModeBase* AuraGameMode = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(WorldContextObject));
+    if (AuraGameMode == nullptr) return nullptr;
+
+    return AuraGameMode->CharacterClassInfo;
 }
---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,96 @@
+// Copyright Pa l'Ull
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include <AuraGameplayTags.h>
+#include <AbilitySystem/Data/CharacterClassInfo.h>
+#include <AbilitySystem/AuraAbilitySystemLibrary.h>
+#include <Interaction/CombatInterface.h>
+
+
+
+struct AuraDamageStatics
+{
+    DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+    DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
+    DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+
+    AuraDamageStatics()
+    {
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, ArmorPenetration, Source, false);
+        DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+    }
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+    static AuraDamageStatics DStatics;
+    return DStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+    RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+    RelevantAttributesToCapture.Add(DamageStatics().ArmorPenetrationDef);
+    RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
+    FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+    const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+    const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+    AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+    AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+    ICombatInterface* SourceCombatInterface = Cast<ICombatInterface>(SourceAvatar);
+    ICombatInterface* TargetCombatInterface = Cast<ICombatInterface>(TargetAvatar);
+    
+
+    const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+    const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+    const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+    FAggregatorEvaluateParameters EvaluationParameters;
+    EvaluationParameters.SourceTags = SourceTags;
+    EvaluationParameters.TargetTags = TargetTags;
+
+    // Get Damage Set by Caller Magnitude
+    float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+
+    // Capture Block Change on Target, and determine if there was a successful Block
+    float TargetBlockChance = 0.f;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
+    TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
+    // If Block, halve the Damage
+    const bool bBlocked = FMath::RandRange(1, 100) < TargetBlockChance;
+    Damage = bBlocked ? Damage / 2.f : Damage;
+
+
+    // Capture Armor on Target
+    float TargetArmor = 0.f;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParameters, TargetArmor);
+    TargetArmor = FMath::Max<float>(TargetArmor, 0.f);
+    // Capture Armor Penetration on Source
+    float SourceArmorPenetration = 0.f;
+    ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef, EvaluationParameters, SourceArmorPenetration);
+    SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration, 0.f);
+
+    const UCharacterClassInfo* CharacterClassInfo = UAuraAbilitySystemLibrary::GetCharacterClassInfo(SourceAvatar);
+    const FRealCurve* ArmorPenetrationCurve = CharacterClassInfo->DamageCalculationCoefficients->FindCurve(FName("ArmorPenetration"), FString());
+    const float ArmorPenetrationCoefficient = ArmorPenetrationCurve->Eval(SourceCombatInterface->GetPlayerLevel());
+
+    /** Armor Penetration Ignores a percentage of the Target's Armor. */
+    const float EffectiveArmor = TargetArmor * (100 - SourceArmorPenetration * ArmorPenetrationCoefficient) / 100.f;
+
+    const FRealCurve* EffectiveArmorCurve = CharacterClassInfo->DamageCalculationCoefficients->FindCurve(FName("EffectiveArmor"), FString());
+    const float EffectiveArmorCoefficient = EffectiveArmorCurve->Eval(TargetCombatInterface->GetPlayerLevel());
+    // One point of Effective Armor reduces 1% of Damage [if we do not scale]
+    Damage *= (100 - EffectiveArmor * EffectiveArmorCoefficient) / 100.f;
+
+    const FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+    OutExecutionOutput.AddOutputModifier(EvaluatedData);
+}
